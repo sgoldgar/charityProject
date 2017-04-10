db57ec906d7421840685eba3995c46e0e661ba32
Juliana Michelsen <julimichelsen@gmail.com>
2017-04-10 09:30:53 -0500

    weekend update

--- frontend/js/donorSearch.js
+++ frontend/js/donorSearch.js
@@ -5,12 +5,52 @@ $(function() {
     console.log( "test!");
 });
 
-$(function () {
-    $('#search').on('keyup', function () {
-        var pattern = $(this).val();
-        $('.items-collection .item').hide();
-        $('.items-collection .item').filter(function () {
-            return $(this).text().match(new RegExp(pattern, 'i'));
-        }).show();
-    });
+// all of this is NOT necessary, bootstrap does it
+
+$('.group-btns .btn').on('click',function(event){
+  event.preventDefault();
+
+  var self = $(this);
+
+          if ( self.find(':checkbox').length > 0 )
+        {
+          //alert('checkbox clicked');
+
+          if ( self.hasClass('selectit' ) )
+          {
+            self.removeClass('selectit');
+			self.addClass('btn-primary');
+          }
+			else
+            {
+            self.addClass('selectit');
+			self.removeClass('btn-primary');
+
+            }
+
+          return;
+        }
+
+            if ( self.find(':radio').length > 0 )
+        {
+          //alert('radiobutton clicked');
+
+
+
+// CHANGE SELECTED ITEM TO GREEN
+
+
+self.siblings("label").addBack().each(function(index, value){
+  $(this).removeClass("selectit");
+    	$(this).addClass("btn-primary");
+});
+
+
+  //alert($(this).text());
+self.removeClass("btn-primary");
+self.addClass("selectit");
+
+        return;
+        }
+
 });
