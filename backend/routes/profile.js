99076848f04096805928d83f22ddec085f57ec3c
Dan <you@example.com>
2017-04-12 16:02:39 -0500

    more lovely routing changes

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -6,14 +6,20 @@ var donatorconn = require('../db/donator_db'),
   donator_model = donatorconn.model('donatorSchema');
   console.log('test');
 
-router.get('/donatorportal', function(req,res,next){
-	
-	donator_model.find({name}, function(err, name){
-		if (err) {console.log(err);}
+router.patch('/donatorProfile', function(req, res, next) {
+   donator_model.findByOne({email: req.body.email}, function(err, name) {
+     if (err) console.log(err);
 
-		res.json(name);
-		console.log(donator_model.name);
+     donator_model.name = req.body.name|| donator_model.name;
+     donator_model.img = req.body.img|| donator_model.img;
+
+donator_model.save(function(err, donator_model) {
+       if (err) console.log(err);
+
+       res.json(donator_model);
+       console.log('donator_model', donator_model);
+    	 });
 	});
-
-
 });
+
+   module.exports = router;
