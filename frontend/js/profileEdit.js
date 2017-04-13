7775b3c76b08688ca064935666f9af7a9ef7075c
Dan <you@example.com>
2017-04-13 16:12:42 -0500

    bug fixes on editprofile.jss

--- frontend/js/profileEdit.js
+++ frontend/js/profileEdit.js
@@ -167,7 +167,7 @@ function pullcharityinfo(){
 }
 
 // This changes the form borders and alerts user to changes in profile
-$('#changesSaved').on('click', function(){
+$('#changesSaved').on('submit', function(){
   console.log('clicked');
   $('.saveInput').css('border', 'none');
   $('#changesSaved').text('Changes Saved').css("background-color", "#00B7D1")
