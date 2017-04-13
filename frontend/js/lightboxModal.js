4456c79ec54817a16b7fa8cd1881ff547d5ec4bf
Dan <you@example.com>
2017-04-13 15:00:51 -0500

    fixed bugs in donation history table

--- frontend/js/lightboxModal.js
+++ frontend/js/lightboxModal.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
 		$('#logInPopup').modal('hide');
 		// $('#sign-in').hide();
 		// // $('.dropButton').show();
-		// window.location.href='charityProfile.html';
+		window.location.href='charityProfile.html';
 	});
 
 	$('#facebookLogSocial').click(function(e){
@@ -42,7 +42,7 @@ $(document).ready(function(){
 		$('#logInPopup').modal('hide');
 		// $('#sign-in').hide();
 		// $('.dropButton').show();
-		//window.location.href='charityProfile.html';
+		window.location.href='charityProfile.html';
 	});
 
 	$('#googleSignSocial').click(function(e){
@@ -50,7 +50,7 @@ $(document).ready(function(){
 		$('#signUpPopup').modal('hide');
 		// $('#sign-in').hide();
 		// $('.dropButton').show();
-		//window.location.href='profileEdit.html';
+		window.location.href='profileEdit.html';
 	});
 
 	$('#facebookSignSocial').click(function(e){
@@ -58,7 +58,7 @@ $(document).ready(function(){
 		$('#signUpPopup').modal('hide');
 		// $('#sign-in').hide();
 		// $('.dropButton').show();
-		// window.location.href='profileEdit.html';
+		window.location.href='profileEdit.html';
 	});
 
 	$('#googleJoinSocial').click(function(e){
@@ -66,7 +66,7 @@ $(document).ready(function(){
 		$('#joinUpPopup').modal('hide');
 		// $('#sign-in').hide();
 		// $('.dropButton').show();
-		//window.location.href='donorProfile.html';
+		window.location.href='donorProfile.html';
 	});
 
 	$('#facebookJoinSocial').click(function(e){
@@ -74,7 +74,7 @@ $(document).ready(function(){
 		$('#joinUpPopup').modal('hide');
 		// $('#sign-in').hide();
 		// $('.dropButton').show();
-		// window.location.href='donorProfile.html';
+		window.location.href='donorProfile.html';
 	});
 
 	$('.close').click(function(e){
